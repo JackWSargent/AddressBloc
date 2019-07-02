ed8adcafc8892a24e95d6874558580ba723a601f
JackWSargent <jackwsargent@gmail.com>
2019-07-02 07:10:13 -0400

    finished listing and searching contacts

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -11,11 +11,13 @@ const ContactController = require("./ContactController");
           choices: [
             "Add new contact",
             "View all contacts",
+            "Search for a contact",
             "Exit"
           ]
         }
       ];
       this.book = new ContactController();
+      
    }
 
    main(){
@@ -28,6 +30,9 @@ const ContactController = require("./ContactController");
          case "View all contacts":
            this.getContacts();
            break;
+         case "Search for a contact":
+            this.search();
+            break;
          case "Exit":
            this.exit();
          default:
@@ -53,6 +58,58 @@ const ContactController = require("./ContactController");
     });
   }
 
+  search(){
+    inquirer.prompt(this.book.searchQuestions)
+    .then((target) => {
+     this.book.search(target.name)
+     .then((contact) => {
+        if(contact === null){
+          this.clear();
+          console.log("contact not found");
+          this.search();
+        } else {
+          this.showContact(contact);
+       }
+
+      });
+   })
+   .catch((err) => {
+     console.log(err);
+     this.main();
+   });
+  }
+
+  showContact(contact){
+    this._printContact(contact);
+    inquirer.prompt(this.book.showContactQuestions)
+    .then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+  }
+
+  _printContact(contact){
+    console.log(`
+      name: ${contact.name}
+      phone number: ${contact.phone}
+      email: ${contact.email}
+      ---------------`
+    );
+  }
+
   getContacts(){
     this.clear();
 
@@ -67,6 +124,24 @@ const ContactController = require("./ContactController");
       }
       this.main();
     }).catch((err) => {
+      console.log(err);
+      this.main();
+    });
+  }
+
+  delete(contact){
+    inquirer.prompt(this.book.deleteConfirmQuestions)
+    .then((answer) => {
+      if(answer.confirmation){
+        this.book.delete(contact.id);
+        console.log("contact deleted!");
+        this.main();
+      } else {
+        console.log("contact not deleted");
+        this.showContact(contact);
+      }
+    })
+    .catch((err) => {
       console.log(err);
       this.main();
     });
